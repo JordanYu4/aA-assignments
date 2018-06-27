3c02722435a9fddd55873ad7922a5a62f8b1e882
Jordan Yu <jordanyu44@gmail.com>
2018-06-26 23:48:41 -0700

    Complete dessert specs up to #serve and #make_more

--- W3D3/RSpec exercise/spec/dessert_spec.rb
+++ W3D3/RSpec exercise/spec/dessert_spec.rb
@@ -6,37 +6,64 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  let(:chef) { double("chef") }
+  let(:chef) { double("chef", name: "Chang") }
+  subject(:mousse) { Dessert.new("fat", 20, :chef) }
 
   describe "#initialize" do
-    it "sets a type"
-
-    it "sets a quantity"
-
-    it "starts ingredients as an empty array"
-
-    it "raises an argument error when given a non-integer quantity"
+    it "sets a type" do
+      expect(mousse.type).to eq("fat")
+    end
+    it "sets a quantity" do
+      expect(mousse.quantity).to eq(20)
+    end
+    it "starts ingredients as an empty array" do
+      expect(mousse.ingredients).to be_empty
+    end
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("fat", "too much", :chef) }.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      expect(mousse.ingredients).to be_empty
+      mousse.add_ingredient("lard")
+      expect(mousse.ingredients).to include("lard")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      mousse.add_ingredient("lard")
+      mousse.add_ingredient("butter")
+      mousse.add_ingredient("bacon grease")
+      ingredient_list = mousse.ingredients.dup
+      mousse.mix!
+      expect(mousse.ingredients).not_to eq(ingredient_list)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
-
-    it "raises an error if the amount is greater than the quantity"
+    it "subtracts an amount from the quantity" do
+      mousse.eat(19)
+      expect(mousse.quantity).to eq(1)
+    end
+    it "raises an error if the amount is greater than the quantity" do
+      expect { mousse.eat(100) }.to raise_error("not enough left!")
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Chef Chang the Great Baker")
+      expect(mousse.serve).to eq("Chef Chang the Great Baker has made 100 fats!")
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(mousse)
+      mousse.make_more
+    end
   end
 end
