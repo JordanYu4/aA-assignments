730c9b7bbf3ac51b4dd111a7f2884d97e1aee118
Jordan Yu <jordanyu44@gmail.com>
2018-07-11 08:54:39 -0700

    Complete Phase I of RailsAuth project

--- W4D3/AuthHomework/app/models/user.rb
+++ W4D3/AuthHomework/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true
+  validates :session_token, presence: true
+  validates :password_digest, presence: true, message: 'Password can\'t be blank'
+  
 end
