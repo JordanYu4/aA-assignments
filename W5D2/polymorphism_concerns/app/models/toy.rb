8b4de3e9e0b46056572065ad3ce761815c9885dc
Jordan Yu <jordanyu44@gmail.com>
2018-07-17 08:40:42 -0700

    Seed database

--- W5D2/polymorphism_concerns/app/models/toy.rb
+++ W5D2/polymorphism_concerns/app/models/toy.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: toys
+#
+#  id           :bigint(8)        not null, primary key
+#  name         :string           not null
+#  toyable_id   :bigint(8)
+#  toyable_type :string
+#
+
 class Toy < ApplicationRecord
   validates :name, uniqueness: { scope: [:toyable] }
   belongs_to :toyable, polymorphic: true
--- W5D2/polymorphism_concerns/test/models/toy_test.rb
+++ W5D2/polymorphism_concerns/test/models/toy_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: toys
+#
+#  id           :bigint(8)        not null, primary key
+#  name         :string           not null
+#  toyable_id   :bigint(8)
+#  toyable_type :string
+#
+
 require 'test_helper'
 
 class ToyTest < ActiveSupport::TestCase
